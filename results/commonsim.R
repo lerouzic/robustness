b1a4e53b1df907d0211a78c51a384ae53440946e
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-01-16 14:49:42 +0100

    Adding various helper functions and sketching figure E (evolution of robustness under different selection pressures).

--- results/commonsim.R
+++ results/commonsim.R
@@ -1,6 +1,7 @@
 sim.path <- "../src/simul.R"
 cache.dir <- "../cache"
 
+library(parallel)
 library(abind)
 
 default.args <- list(
@@ -22,14 +23,15 @@ default.args <- list(
 	lateenv.sd=0,
 	theta=0.5,
 	s=10,
+	ss=0,
 	test.rep=100,
 	test.initenv.sd=1,
 	test.lateenv.sd=0.1,
 	test.initmut.sd=0.1,
 	test.lateenv.sd=0.1,
 	test.indiv=FALSE)
-
-sim.run <- function(args=default.args, sim.name=NA, force.run=FALSE, nice=TRUE, verbose=FALSE) {
+	
+sim.run.single <- function(args=default.args, sim.name=NA, force.run=FALSE, nice=TRUE, verbose=FALSE) {
 	myargs <- default.args
 	myargs[names(args)] <- args # Mistakes in arg names etc. will make the simulation crash later
 	if (is.na(sim.name)) 
@@ -47,20 +49,38 @@ sim.run <- function(args=default.args, sim.name=NA, force.run=FALSE, nice=TRUE, 
 	ans
 }
 
+sim.run.reps <- function(args=NULL, reps=10, series.name="sim", force.run=FALSE, nice=TRUE, verbose=FALSE, mc.cores=detectCores()-1) {
+	ans <- mclapply(1:reps, function(r) {
+		ss <- sim.run.single(args, paste0(series.name, "-", r), force.run=force.run, nice=nice, verbose=verbose)
+	}, mc.cores=mc.cores)
+
+	ans.mean <- acrossrepFUN(ans, mean)
+	ans.sd   <- acrossrepFUN(ans, sd)
+	list(full=ans, mean=ans.mean, sd=ans.sd)
+}
+
 acrossrepFUN <- function(simres, FUN=mean, ...) {
 	xa <- abind(simres, along=3)
 	apply(xa, 1:2, FUN, ...)
 }
 
-plotts <- function(mm, sd=NULL, sd.factor=1, colname, xlab="Generation", ylab=colname, col="black", ylim=NULL, xlim=NULL, ...) {
+lighten.color <- function(color, factor=0.3){
+    col <- col2rgb(color)
+    col <- col+(255-col)*factor
+    col <- rgb(t(col), maxColorValue=255)
+    col
+}
+
+
+plotts <- function(mm, sd=NULL, sd.factor=1, colname, xlab="Generation", ylab=colname, col="black", ylim=NULL, xlim=NULL, add=FALSE, type="l", ...) {
 	stopifnot(colname %in% colnames(mm))
-	if(dev.cur() == 1) { # no graphical device
+	if(dev.cur() == 1 || !add) { # no graphical device
 		if (is.null(xlim)) xlim <- range(as.numeric(rownames(mm)))
 		if (is.null(ylim)) ylim <- range(mm[,colname]) + if(is.null(sd)) 0 else sd.factor*c(-1,1)*max(sd[,colname])
 		plot(NULL, xlim=xlim, ylim=ylim, xlab=xlab, ylab=ylab, ...)
 	}
-	lines(x=as.numeric(rownames(mm)), y=mm[,colname], col=col, ...)
 	if (!is.null(sd)) {
-		arrows(x0=as.numeric(rownames(mm)), y0=mm[,colname]-sd.factor*sd[,colname], y1=mm[,colname]+sd.factor*sd[,colname], lty=1, col=col, length=0)
+		arrows(x0=as.numeric(rownames(mm)), y0=mm[,colname]-sd.factor*sd[,colname], y1=mm[,colname]+sd.factor*sd[,colname], lty=1, col=lighten.color(col), length=0)
 	}
+	lines(x=as.numeric(rownames(mm)), y=mm[,colname], col=col, type=type, ...)
 }
--- results/figE.R
+++ results/figE.R
@@ -5,5 +5,53 @@ source("./commonsim.R")
 library(parallel)
 mc.cores <- min(64, detectCores()-1)
 
-# Standard stabilizing selection
-ss <- sim.run(default.args, "try1", force.run=TRUE)
+force.run=TRUE
+
+reps <- 6
+G <- 1000
+N <- 1000
+summary.every <- 20
+randtheta <- function() paste(round(runif(default.args$n, 0, 1), digits=3), collapse=" ")
+
+stab <- sim.run.reps(args <- list(G=G, N=N, summary.every=summary.every, theta=randtheta()),reps=reps, series.name="stab", force.run=force.run)
+stabty <- sim.run.reps(args <- list(G=G, N=N, summary.every=summary.every, theta=randtheta(), ss=36000), reps=reps, series.name="stabty", force.run=force.run)
+som <- sim.run.reps(args <- list(G=G, N=N, summary.every=summary.every, theta=randtheta(), som.rate=1, som.sd=0.1),reps=reps, series.name="som", force.run=force.run)
+ienv <- sim.run.reps(args <- list(G=G, N=N, summary.every=summary.every, theta=randtheta(), initenv.sd=1), reps=reps, series.name="ienv", force.run=force.run)
+lenv <- sim.run.reps(args <- list(G=G, N=N, summary.every=summary.every, theta=randtheta(), lateenv.sd=0.1), reps=reps, series.name="lenv", force.run=force.run)
+
+plotts(stab$mean, stab$sd, colname="fitness.mean", lwd=3, ylim=c(0,1))
+plotts(stabty$mean, stabty$sd, colname="fitness.mean", lwd=3, add=TRUE, col="blue")
+plotts(som$mean, som$sd, colname="fitness.mean", lwd=3, add=TRUE, col="brown")
+plotts(ienv$mean, ienv$sd, colname="fitness.mean", lwd=3, add=TRUE, col="orange")
+plotts(lenv$mean, lenv$sd, colname="fitness.mean", lwd=3, add=TRUE, col="darkgreen")
+
+plotts(stab$mean, stab$sd, colname="robustness.initmut.mean", lwd=3, ylim=c(0,0.004))
+plotts(stabty$mean, stabty$sd, colname="robustness.initmut.mean", lwd=3, add=TRUE, col="blue")
+plotts(som$mean, som$sd, colname="robustness.initmut.mean", lwd=3, add=TRUE, col="brown")
+plotts(ienv$mean, ienv$sd, colname="robustness.initmut.mean", lwd=3, add=TRUE, col="orange")
+plotts(lenv$mean, lenv$sd, colname="robustness.initmut.mean", lwd=3, add=TRUE, col="darkgreen")
+
+plotts(stab$mean, stab$sd, colname="robustness.latemut.mean", lwd=3, ylim=c(0,0.0015))
+plotts(stabty$mean, stabty$sd, colname="robustness.latemut.mean", lwd=3, add=TRUE, col="blue")
+plotts(som$mean, som$sd, colname="robustness.latemut.mean", lwd=3, add=TRUE, col="brown")
+plotts(ienv$mean, ienv$sd, colname="robustness.latemut.mean", lwd=3, add=TRUE, col="orange")
+plotts(lenv$mean, lenv$sd, colname="robustness.latemut.mean", lwd=3, add=TRUE, col="darkgreen")
+
+plotts(stab$mean, stab$sd, colname="robustness.initenv.mean", lwd=3, ylim=c(0,0.0001))
+plotts(stabty$mean, stabty$sd, colname="robustness.initenv.mean", lwd=3, add=TRUE, col="blue")
+plotts(som$mean, som$sd, colname="robustness.initenv.mean", lwd=3, add=TRUE, col="brown")
+plotts(ienv$mean, ienv$sd, colname="robustness.initenv.mean", lwd=3, add=TRUE, col="orange")
+plotts(lenv$mean, lenv$sd, colname="robustness.initenv.mean", lwd=3, add=TRUE, col="darkgreen")
+
+plotts(stab$mean, stab$sd, colname="robustness.lateenv.mean", lwd=3, ylim=c(0,0.003))
+plotts(stabty$mean, stabty$sd, colname="robustness.lateenv.mean", lwd=3, add=TRUE, col="blue")
+plotts(som$mean, som$sd, colname="robustness.lateenv.mean", lwd=3, add=TRUE, col="brown")
+plotts(ienv$mean, ienv$sd, colname="robustness.lateenv.mean", lwd=3, add=TRUE, col="orange")
+plotts(lenv$mean, lenv$sd, colname="robustness.lateenv.mean", lwd=3, add=TRUE, col="darkgreen")
+
+plotts(stab$mean, stab$sd, colname="robustness.stability.mean", lwd=3, ylim=c(0,0.0001))
+plotts(stabty$mean, stabty$sd, colname="robustness.stability.mean", lwd=3, add=TRUE, col="blue")
+plotts(som$mean, som$sd, colname="robustness.stability.mean", lwd=3, add=TRUE, col="brown")
+plotts(ienv$mean, ienv$sd, colname="robustness.stability.mean", lwd=3, add=TRUE, col="orange")
+plotts(lenv$mean, lenv$sd, colname="robustness.stability.mean", lwd=3, add=TRUE, col="darkgreen")
+

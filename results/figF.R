28f1b78218f670561d3dd09cce53c3c1c11a8749
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-03-03 13:46:33 +0100

    Removing test options from fig F.

--- results/figF.R
+++ results/figF.R
@@ -18,17 +18,17 @@ force.run=FALSE
 #~ summary.every <- 1000
 
 ###### Specific for tests
-reps <- 2
-G <- 100
-N <- 1000
-summary.every <- 10
-force.run <- TRUE
+#~ reps <- 2
+#~ G <- 100
+#~ N <- 1000
+#~ summary.every <- 10
+#~ force.run <- TRUE
 ######
 
 nselgen <- 3
 new.s <- paste(c(rep(default.args$s, nselgen), rep(0, default.args$n-nselgen)), collapse=" ")
 randtheta <- function() paste(round(runif(default.args$n, 0, 1), digits=3), collapse=" ")
-stab <- sim.run.reps(args <- list(G=G, N=N, summary.every=summary.every, theta=randtheta(), s=new.s),reps=reps, series.name="stabtest", force.run=force.run, mc.cores=mc.cores)
+stab <- sim.run.reps(args <- list(G=G, N=N, summary.every=summary.every, theta=randtheta(), s=new.s),reps=reps, series.name="stab", force.run=force.run, mc.cores=mc.cores)
 #~ istabty <- sim.run.reps(args <- list(G=G, N=N, summary.every=summary.every, theta=randtheta(), s=new.s, ss=36000), reps=reps, series.name="stabty", force.run=force.run, mc.cores=mc.cores)
 #~ som <- sim.run.reps(args <- list(G=G, N=N, summary.every=summary.every, theta=randtheta(), s=new.s, som.rate=1, som.sd=0.1),reps=reps, series.name="som", force.run=force.run, mc.cores=mc.cores)
 #~ ienv <- sim.run.reps(args <- list(G=G, N=N, summary.every=summary.every, theta=randtheta(), s=new.s, initenv.sd=0.1), reps=reps, series.name="ienv", force.run=force.run, mc.cores=mc.cores)
@@ -46,10 +46,10 @@ for (ssi in seq_along(stab$full))
 
 
 # All sims
-plotmat(robindex.Gmatrix.outfile(stab$mean), arg1=2, arg2=3, col="blue", col.end="green", var.thresh=1e-20)
+plotmat(robindex.Gmatrix.outfile(stab$mean[-1,]), arg1=1, arg2=2, col="blue", col.end="green", var.thresh=1e-20)
 
 # For the M matrix, one must compute average M matrices instead
 plotmat(lapply(rownames(stab$mean), function(gg) 
 	list(mean=colMeans(do.call(rbind, lapply(listM, function(mm) mm[[gg]]$mean))),
 	     vcov=matrix(colMeans(do.call(rbind, lapply(listM, function(mm) c(mm[[gg]]$vcov)))), ncol=5))),
-	     arg1=2, arg2=3, col="blue", col.end="green", var.thresh=1e-20)
+	     arg1=3, arg2=4, col="blue", col.end="green", var.thresh=1e-20, asp=1)

f52ab2eb73334ac72245a47455fb4d9926bb45cf
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-03-03 17:02:38 +0100

    Minor visual improvements.

--- results/figB.R
+++ results/figB.R
@@ -1,4 +1,6 @@
+#!/usr/bin/env Rscript
 
+# Runs a PCA among robustness indexes and plots the results
 
 source("./terminology.R")
 
@@ -9,8 +11,10 @@ phen <- c(
     initmut=TERM.GENCAN.LONG,
     latemut=TERM.SOM.LONG,
     stability=TERM.STAB.LONG)
+    
+phen.pos.ref <- "initmut"
 
-lowthresh <- 1e-4
+lowthresh <- -40
 
 # This script uses the same data as figA. Run figA before. 
 cache.dir <- "../cache"
@@ -31,8 +35,11 @@ myplot.prcomp <- function(pr,
     nPC <- length(pr$sdev)
     layout(t(1:2))
     
+    # In prcomp, the sign of the PCs is arbitrary. To ensure reproducibility, PCs will be oriented in such a was that phen.pos.ref > 0
+    pr$rotation <- t(t(pr$rotation)*sign(pr$rotation[phen.pos.ref,]))
+    
     plot(NULL, xlab="", ylab="", ylim=c(0.75,nPC+0.25), xlim=range(pr$rotation), yaxt="n", bty="n")
-    arrows(x0=min(pr$rotation), y0=1:nPC, x1=max(pr$rotation), code=3)
+    arrows(x0=min(pr$rotation), y0=1:nPC, x1=max(pr$rotation), code=3, length=0.2)
     for (i in 1:nPC) text(x=pr$rotation[,i], y=nPC-i+1+0.1*(0:(nPC-1)), labels[rownames(pr$rotation)], col=cols[rownames(pr$rotation)], pos=3, cex=0.8, font=2)
     lines(x=rep(0,2), y=c(1,nPC), lty=2)
     axis(2, at=1:nPC, labels=paste0("PC", nPC:1))
@@ -41,15 +48,13 @@ myplot.prcomp <- function(pr,
     
 }
 
-logpca <- TRUE
-genepca <- FALSE
+
+whattoconsider<- function(x) x[[1]] # the first gene of the network
+whattoconsider <- function(x) mean(x) # the average index for all genes
+
 pdf("figB.pdf", width=7, height=7)
 
-rrr <- do.call(rbind, lapply(dd, function(ddd) sapply(names(phen), function(ppp) ddd[[ppp]][1])))
-if(logpca) {
-    rrr[rrr< lowthresh] <- lowthresh
-    rrr <- log(rrr)
-}
+rrr <- do.call(rbind, lapply(dd, function(ddd) sapply(names(phen), function(ppp) whattoconsider(ddd[[ppp]]))))
 
 prp <- prcomp(rrr, scale.=TRUE)
 myplot.prcomp(prp)

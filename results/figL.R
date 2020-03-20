080d5f94fb49e61be1f8f741f7d2e2ce11b7fc4c
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-03-20 09:42:28 +0100

    Generating a list of functions was a bit more complicated than that (bug with the number of genes). Attempt to fix it.

--- results/figL.R
+++ results/figL.R
@@ -34,25 +34,25 @@ selg.values  <- 1:6
 s.values <- 10^seq(-2, 4, length.out=nb.values)
 
 torun.mut <- lapply(mut.values, function(mm) 
-	function() pure.run.reps(W0, list(s=s, G=G, N=N, rep=test.rep, summary.every=G, mut.rate=mm), 
-		reps=reps, series.name=paste0("real-ref-m", signif(mm, digits=2)), force.run=force.run))
+	substitute(function() pure.run.reps(W0, list(s=s, G=G, N=N, rep=test.rep, summary.every=G, mut.rate=mm), 
+		reps=reps, series.name=paste0("real-ref-m", signif(mm, digits=2)), force.run=force.run), list(mm=mm)))
 torun.N <- lapply(N.values, function(nn) 
-	function() pure.run.reps(W0, list(s=s, G=G, N=nn, rep=test.rep, summary.every=G, mut.rate=mut.rate), 
-		reps=reps, series.name=paste0("real-ref-N", nn), force.run=force.run))
+	substitute(function() pure.run.reps(W0, list(s=s, G=G, N=nn, rep=test.rep, summary.every=G, mut.rate=mut.rate), 
+		reps=reps, series.name=paste0("real-ref-N", nn), force.run=force.run), list(nn=nn)))
 torun.genes <- lapply(genes.values, function(gg) 
-	function() pure.run.reps(matrix(rnorm(gg^2, sd=0.000001), ncol=gg), list(s=s, G=G, N=N, rep=test.rep, summary.every=G, mut.rate=mut.rate), 
-		reps=reps, series.name=paste0("real-ref-g", gg), force.run=force.run))
+	substitute(function() pure.run.reps(W0=matrix(rnorm(gg^2, sd=0.000001), ncol=gg), list(s=c(rep(10, sel.genes), rep(0 ,gg-sel.genes)), G=G, N=N, rep=test.rep, summary.every=G, mut.rate=mut.rate), 
+		reps=reps, series.name=paste0("real-ref-g", gg), force.run=force.run), list(gg=gg)))
 torun.selg <- lapply(selg.values, function(sg)
-	function() pure.run.reps(W0, list(s=c(rep(10,sg),rep(0,n.genes-sg)), G=G, N=N, rep=test.rep, summary.every=G, mut.rate=mut.rate), 
-		reps=reps, series.name=paste0("real-ref-sg", sg), force.run=force.run))
+	substitute(function() pure.run.reps(W0, list(s=c(rep(10,sg),rep(0,n.genes-sg)), G=G, N=N, rep=test.rep, summary.every=G, mut.rate=mut.rate), 
+		reps=reps, series.name=paste0("real-ref-sg", sg), force.run=force.run), list(sg=sg)))
 torun.sel <- lapply(s.values, function(ss)
-	function() pure.run.reps(W0, list(s=c(rep(ss,sel.genes),rep(0,n.genes-sel.genes)), G=G, N=N, rep=test.rep, summary.every=G, mut.rate=mut.rate), 
-		reps=reps, series.name=paste0("real-ref-s", signif(ss, digits=2)), force.run=force.run))	
+	substitute(function() pure.run.reps(W0, list(s=c(rep(ss,sel.genes),rep(0,n.genes-sel.genes)), G=G, N=N, rep=test.rep, summary.every=G, mut.rate=mut.rate), 
+		reps=reps, series.name=paste0("real-ref-s", signif(ss, digits=2)), force.run=force.run), list(ss=ss)))
 
 torun <- setNames(
 	c(torun.mut, torun.N, torun.genes, torun.selg, torun.sel),
 	c(paste0("ref.m", signif(mut.values, digits=2)), paste0("ref.N", N.values), paste0("ref.g", genes.values), paste0("ref.sg", selg.values), paste0("ref.s", signif(s.values, digits=2))))
-list.sim <- mclapply(torun, function(ff) ff(), mc.cores=min(length(torun), ceiling(mc.cores/reps)))
+list.sim <- mclapply(torun, function(ff) eval(ff)(), mc.cores=min(length(torun), ceiling(mc.cores/reps)))
 
 layout(matrix(1:25, ncol=5))
 for (par in c("m","N","g","sg","s")) {

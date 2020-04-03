10346070f50485dea3ed59f6337872274cb0baac
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-04-03 13:26:38 +0200

    Change one of the study cases to make it more characteristic (fully-robust).

--- results/studycases.R
+++ results/studycases.R
@@ -21,7 +21,7 @@ targetW <- function(W, target, a) {
 # Study cases
 stud <- rbind(
     A=c(0.7,0.2),
-    B=c(-0.3,0.3),
+    B=c(0.15,0.5),
     C=c(-0.4,0.8),
     D=c(-1,-0.8),
     E=c(1.5,3.5))

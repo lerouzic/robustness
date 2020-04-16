2099528afca1bd89ecc0dcb2f130ee11b2b05a1c
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-04-16 11:29:21 +0200

    Improvement of the code structure in randnetwork.R + simplification of function names.

--- results/randnetwork.R
+++ results/randnetwork.R
@@ -1,6 +1,6 @@
 
 
-randW.default <- function(net.size, reg.mean, reg.sd, density = 1) {
+randW <- function(net.size, reg.mean, reg.sd, density = 1) {
 	# Returns a random matrix of specified size, mean, sd, and density. 
 	# Organised in such a way as at least one element in each line is non zero
 	numzeros <- min(floor((1-density)*net.size^2), net.size*(net.size-1))
@@ -11,24 +11,27 @@ randW.default <- function(net.size, reg.mean, reg.sd, density = 1) {
 	t(W)
 }
 
-Wdist.fromWlist <- function(Wlist, epsilon.zero = 0.1) {
+Wdist.fromWlist <- function(Wlist, epsilon.zero = 0.01) {
 	uu <- unlist(Wlist)
 	list(
-		reg.mean = mean(uu[abs(uu) >= epsilon.zero),
-		reg.sd   = sd(uu[abs(uu) >= epsilon.zero),
-		density  = mean(abs(uu) < epsilon.zero)
+		mean = mean(uu[abs(uu) >= epsilon.zero]),
+		sd   = sd(uu[abs(uu) >= epsilon.zero]),
+		density  = mean(abs(uu) > epsilon.zero)
 	)
 }
 
-Wdist.fromfiles <- function(files, gen = NA, epsilon.zero = 0.1, cols = NA, rows = NA) {
-	Wdift.fromWlist(lapply(files, function(ff) {
+Wlist.fromfiles <- function(files, gen = NA, cols = NA, rows = NA) {
+	lapply(files, function(ff) {
 		ss <- readRDS(ff)
-		if (is.na(gen) || !as.character(gen) %in% names(ss)) gen <- rownames(ss)[nrow(ss)]
+		if (is.na(gen) || !as.character(gen) %in% names(ss)) gen <- names(ss)[length(ss)]
 		W <- ss[[as.character(gen)]]$W
 		if (is.na(cols)) cols <- 1:ncol(W)
 		if (is.na(rows)) rows <- 1:nrow(W)
 		stopifnot(all(cols %in% 1:ncol(W)), all(rows %in% 1:nrow(W)))
 		W[rows, cols]
-	}), 
-	epsilon.zero=epsilon.zero)
+	})
+} 
+
+Wdist.fromfiles <- function(files, gen = NA, epsilon.zero = 0.1, cols = NA, rows = NA) {
+	Wdist.fromWlist(Wlist.fromfiles(files=files, gen=gen, cols=cols, rows=rows), epsilon.zero=epsilon.zero)
 } 

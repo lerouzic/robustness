373c1dc04e38af35f2ef1f2a706d9c71d677aba5
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2020-09-04 15:09:10 +0200

    Forgot commonpure.R in commit b462990d609.

--- results/commonpure.R
+++ results/commonpure.R
@@ -20,7 +20,7 @@ default.args <- list(
 	sim.initenv.sd  = 0,
 	sim.lateenv.sd  = 0,
 	mut.sd          = default.sim.mutsd,
-	theta           = rep(NA, default.n),
+	theta           = NA,
 	s               = default.s,
 	grad.rob        = rep(0, 5),
 	rep             = default.rob.reps,
@@ -47,9 +47,17 @@ acrossrepVar <- function(fulllist) {
 pure.run.single <- function(W0, myargs=NULL, sim.name=NA, force.run=FALSE) {
 	missing.args <- names(default.args)[!(names(default.args) %in% names(myargs))]
 	myargs[missing.args] <- default.args[missing.args]
+	# Here the algorithm gets a bit complex, the problem is due to the fact that the number of genes can be deduced from different sources.
+	# If W0 is NA, the W matrix needs to be built from another source of information (the length of theta). 
+	# If W0 is a matrix, then we assume that the number of columns give the number of genes, and theta is updated accordingly
+	# If both W0 and theta are NA, then the network is assumed to have only one gene. 
+	if (ncol(W0) > 1) {
+		if (length(myargs$theta) > ncol(W0)) myargs$theta <- myargs$theta[1:ncol(W0)]
+		if (length(myargs$theta) < ncol(W0)) myargs$theta <- c(myargs$theta, rep(NA, ncol(W0)-length(myargs$theta)))
+	}
 	# NAs in the optimum are replaced by random values
 	myargs$theta[is.na(myargs$theta)] <- runif(sum(is.na(myargs$theta)))
-	if (is.na(W0)) {
+	if (all(is.na(W0))) {
 		# NA for W0 is replaced by a random matrix at the correct equilibrium
 		WW <- matrix(rnorm(length(myargs$theta)^2, default.rand.mean, default.rand.sd.sim), ncol=length(myargs$theta))
 		for (i in 1:nrow(WW)) WW[i,sample(1:ncol(WW), 1)] <- NA
